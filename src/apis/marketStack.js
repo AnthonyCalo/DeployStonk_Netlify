5abdd132ff2d15777c2939ff507c335dd510923b
Anthony Calo <acalo4554@gmail.com>
2021-09-21 12:39:42 -0400

    api update

--- src/apis/marketStack.js
+++ src/apis/marketStack.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const { REACT_APP_MARKETSTACK } = process.env;
 
 export default axios.create({
-    baseURL: 'http://api.marketstack.com/v1/',
+    baseURL: 'https://api.marketstack.com/v1/',
     params: {
         access_key: REACT_APP_MARKETSTACK
     }
